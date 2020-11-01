5aff4fe065c17140e786cf771e662f117c4cee0f
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-01 08:02:11 +0200

    add changes

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -6,26 +6,3 @@ import { Quote } from '../quote';
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
-export class QuoteDetailsComponent implements OnInit {
-
-  @Input() Quote:Quote;
-  @Output() complete = new EventEmitter<boolean>();
-
-  quoteDelete(deletequt:boolean) {
-    this.complete.emit(deletequt);
-  }
-
-  upVote() {
-    this.Quote.upVote ++
-  }
-
-  downVote() {
-    this.Quote.downVote ++
-  }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
